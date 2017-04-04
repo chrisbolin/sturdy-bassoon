cf8c177385e2eb539022b8355e9db22ce865733d
Chris Bolin <bolin.chris@gmail.com>
2017-04-04 23:49:46 +0800

    basic grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { range } from 'lodash';
 import './App.css';
+
+const Cell = () => (
+  <div className="Cell"/>
+)
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="Grid">
+          {range(16 * 16).map(i => <Cell key={i} />)}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
